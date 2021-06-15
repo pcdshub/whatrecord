233fb8aefd9d4990a100f64f6e1781e035e0730f
Ken Lauer <klauer@slac.stanford.edu>
2021-06-15 14:54:48 -0700

    MNT: use API_HOST/API_PORT env vars

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
         '^/api': {
-            target: 'http://localhost:8898/'
+            target: 'http://' + process.env.API_HOST + ':' + process.env.API_PORT + '/'
         }
     }
   }
