20336965b02c1394e11290d4922d453b7bfe70d2
Ken Lauer <klauer@slac.stanford.edu>
2021-08-11 08:56:23 -0700

    MNT: fancy nullish coalescing operator

--- frontend/src/views/twincat_pytmc.vue
+++ frontend/src/views/twincat_pytmc.vue
@@ -72,7 +72,7 @@
                 :placeholder="`Filter by name`" />
             </template>
           </Column>
-          <Column field="type" header="Type" :sortable="true" style="width: 15vw">
+          <Column field="type" header="Type" :sortable="true" style="width: 20vw">
             <template #filter="{filterModel,filterCallback}">
               <InputText type="text" v-model="filterModel.value" @keydown.enter="filterCallback()" class="p-column-filter"
                 :placeholder="`Filter by type`" />
@@ -138,18 +138,13 @@ export default {
       }
       return this.plc_info.metadata_by_key[this.item_name];
     },
-    record_to_metadata_keys() {
-      if (!this.info_ready) {
-        return {};
-      }
-      return this.plc_info.record_to_metadata_keys || {};
-    },
+
     related_records() {
       if (!this.item_name) {
         return [];
       }
       let records = [];
-      for (const [record, symbols] of Object.entries(this.record_to_metadata_keys)) {
+      for (const [record, symbols] of Object.entries(this.plc_info.record_to_metadata_keys || {})) {
         for (const symbol of symbols) {
           if (symbol == this.item_name) {
             records.push(record);
@@ -158,11 +153,9 @@ export default {
       }
       return records;
     },
+
     symbols () {
-      if (Object.keys(this.plc_info).length == 0) {
-        return [];
-      }
-      return Object.values(this.plc_info.metadata_by_key).map(
+      return Object.values(this.plc_info?.metadata_by_key ?? {}).map(
         function (info) {
           let obj = { ...info };
           obj.full_name = info.name;
@@ -179,38 +172,24 @@ export default {
     },
 
     plc_dependencies () {
-      if (!this.info_ready) {
-        return {};
-      }
-      return this.plc_info.metadata.dependencies;
+      return this.plc_info?.metadata?.dependencies ?? [];
     },
 
     ...mapState({
       plcs (state) {
-        if (!this.plcs_ready) {
-          return [];
-        }
-        return state.plugin_nested_info.twincat_pytmc.keys;
+        return state.plugin_nested_info.twincat_pytmc?.keys ?? [];
       },
 
       plcs_ready (state) {
-        const nested_info = state.plugin_nested_info;
-        const pytmc_info = nested_info.twincat_pytmc;
-        return pytmc_info && pytmc_info.keys.length;
+        return (state.plugin_nested_info.twincat_pytmc?.keys.length > 0) ?? false;
       },
 
       plc_info (state) {
-        if (!this.info_ready) {
-          return {};
-        }
-        const pytmc_info = state.plugin_nested_info.twincat_pytmc;
-        return pytmc_info.info[this.selected_plc_name];
+        return state.plugin_nested_info.twincat_pytmc?.info[this.selected_plc_name] ?? {};
       },
 
       info_ready (state) {
-        const nested_info = state.plugin_nested_info;
-        const pytmc_info = nested_info.twincat_pytmc;
-        return (this.selected_plc_name && pytmc_info && this.selected_plc_name in pytmc_info.info);
+        return (this.selected_plc_name && this.selected_plc_name in (state.plugin_nested_info.twincat_pytmc?.info ?? {}));
       },
 
     }),
@@ -236,13 +215,15 @@ export default {
       const route_query = this.$route.query;
       const plc = route_query.plc || "";
       // const item_name = route_query.item_name || "";
-      await this.$store.dispatch(
-        "get_plugin_nested_info", {
-          plugin: "twincat_pytmc",
-          key: plc,
-        }
-      );
-      this.selected_plc = { plc: plc };
+      if (plc) {
+        await this.$store.dispatch(
+          "get_plugin_nested_info", {
+            plugin: "twincat_pytmc",
+            key: plc,
+          }
+        );
+        this.selected_plc = { plc: plc };
+      }
     },
 
     init_filters() {
