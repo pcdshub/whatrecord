13c5f7c688c2379e97ee1c4685940b9b11afcdb4
Ken Lauer <klauer@slac.stanford.edu>
2023-05-24 15:38:22 -0700

    MNT: missing start_frontend.sh

--- docker/support/start_frontend.sh
+++ docker/support/start_frontend.sh
@@ -4,9 +4,8 @@ WHATRECORD_FRONTEND_PORT=${WHATRECORD_FRONTEND_PORT-8896}
 
 echo "* Installing frontend dependencies..."
 yarn install
-echo "* Performing first build of frontend pages"
-yarn build
 echo "* Monitoring frontend files in the background to rebuild automatically when they update."
+echo "* Note: it may take a few seconds before the pages are ready to be served"
 yarn build --watch &
 echo "* Running the development server on port ${WHATRECORD_FRONTEND_PORT}."
 yarn serve --port "${WHATRECORD_FRONTEND_PORT}" --host --strictPort  # --debug --logLevel debug
